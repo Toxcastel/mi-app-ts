0ad857fb0efa185acacbb7d5ef6b8a90590c33d4
albertorapu@gmail.com <albertorapu@gmail.com>
2022-11-16 11:38:13 -0300

    Creating User command

---
+++ src/components/Usuario.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+/*
+    Aquí la pregunta es: ¿cómo establecemos en el Login la información del usuario?
+    Para esto en TypeScript debemos trabajar con clases o con interfaces para asignar el tipado
+    interface: reglas que podemos ponerle a los objetos, no tienen una interpretación física en js
+*/
+
+interface User {
+    uid: string;
+    name: string;
+}
+
+const Usuario = () => {
+    /* En TypeScript hay funciones con definición genérica como el 'useState' que podría manejar internamente "interfaces", es decir, objetos de JS que indican el tipado de cada propiedad del objeto */
+    const [user, setUser] = useState<User>();
+
+    const login = () => {
+        setUser({
+            uid: "ABC123",
+            name: "Fernando Herrera",
+        });
+    };
+    return (
+        <div className="mt-5">
+            <h3>Usuario: useState</h3>
+            <button onClick={login} className="btn btn-outline-primary">
+                Login
+            </button>
+            {/* <pre> se refiere a texto preformateado, se imprimirá tal como lo veamos. Usamos JSON.stringify para mostrar como string el valor del objeto user */}
+            <pre> {JSON.stringify(user)}</pre>
+        </div>
+    );
+};
+
+export default Usuario;
