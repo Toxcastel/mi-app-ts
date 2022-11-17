ca220ba8317b20f31e0ca6f1137711d44bcad4cd
albertorapu@gmail.com <albertorapu@gmail.com>
2022-11-17 11:19:03 -0300

    custom hooks: useForm

---
+++ src/components/FormularioCustomHook.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent, useState } from "react";
+import { useForm } from "../hooks/useForm";
+
+export const FormularioCustomHook = () => {
+    // const [formulario, setFormulario] = useState({
+    //     email: "",
+    //     nombre: "",
+    // });
+
+    // /* Los eventos onChange son de tipo "ChangeEvent" en TS y dentro de ellos hay que especificar también el tipo interno. En el caso de los formularios, como lo es en este caso, debemos usar <HTMLInputElement> */
+    // const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    //     const { name, value } = target;
+
+    //     /* Es recomendable destructurar el valor actual del formulario al momento de setear un nuevo state, ya que puede tener más variables que las iniciales */
+    //     setFormulario({
+    //         ...formulario,
+    //         [name]: value
+    //     })
+    // };
+
+    const {formulario, handleChange} = useForm({email:'', nombre:''})
+
+    return (
+        <form autoComplete="off">
+            <div className="mb-3">
+                <label className="form-label">Email:</label>
+                <input type="email" className="form-control" name="email" onChange={handleChange} />
+            </div>
+
+            <div className="mb-3">
+                <label className="form-label">Nombre:</label>
+                <input type="text" className="form-control" name="nombre" onChange={handleChange}/>
+            </div>
+
+            <pre>{JSON.stringify(formulario)}</pre>
+        </form>
+    );
+};
